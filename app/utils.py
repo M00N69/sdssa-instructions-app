460531f687e7d40c04be0172f73a6d3bb7e2ca37
MOON <133333114+M00N69@users.noreply.github.com>
2025-02-10 22:45:30 +0100

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,1 +1,11 @@
+import sqlite3
+import pandas as pd
+
+def load_data():
+    """Charge les données depuis la base de données SQLite."""
+    conn = sqlite3.connect('data/sdssa_instructions.db')
+    query = "SELECT * FROM instructions"
+    df = pd.read_sql_query(query, conn)
+    conn.close()
+    return df
 
